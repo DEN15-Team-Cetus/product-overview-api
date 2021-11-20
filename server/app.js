4ff74817ac86f4278625a8c0c144770005bfb7ca
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-20 13:19:01 -0600

    Successfully refactored getRelatedProducts route to rely mainly on SQL

--- server/app.js
+++ server/app.js
@@ -1,12 +1,6 @@
-var express = require('express');
-var db = require('./index.js');
-var app = express();
+/* eslint-disable import/extensions */
+const express = require('express');
+const db = require('./index.js');
+const app = express();
 
 module.exports.app = app;
-
-// app.set('port', 3000);
-
-// app.listen(3000, () => {
-//   console.log('Now listening on port 3000');
-// });
-
--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+/* eslint-disable import/extensions */
 const path = require('path');
 const express = require('express');
-const app = express();
 const db = require('../database/index.js');
+
+const app = express();
+// const { response } = require('express');
 
 app.set('port', 3000);
 app.use(express.json()); // Allows us to parse json
 
-// Return a list of products
+// Return list of products
 app.get('/products', (req, res) => {
-  let count = req.query.count ? req.query.count : 5;
-  let page = req.query.page ? req.query.page : 1;
-  let offset = ((page - 1) * count); // Specify which row to start selecting from
+  const count = req.query.count ? req.query.count : 5;
+  const page = req.query.page ? req.query.page : 1;
+  const offset = ((page - 1) * count); // Specify which row to start selecting from
   db.query(`SELECT * FROM products LIMIT ${offset}, ${count}`, (err, data) => {
-    if(err) {
+    if (err) {
       console.log(err);
     }
     res.send(data);
@@ -20,28 +26,85 @@ app.get('/products', (req, res) => {
 });
 
 // Return all product level info for specified product_id
-// TODO: Need to include features array in the response object
 app.get('/products/:product_id', (req, res) => {
-  db.query(
-    `SELECT *,
-      (SELECT JSON_ARRAY(
-        JSON_OBJECT('feature', 4000, 'value', 9999)
-      )) AS features
-    FROM products WHERE id = ${req.params.product_id}`, (err, data) => {
-    if(err) {
+  let productInfo = {};
+  db.query(`SELECT * FROM products WHERE id = ${req.params.product_id}`, (err, data) => {
+    if (err) {
       console.log(err);
     }
-    res.send(data);
+    // Populate productInfo
+    for (let key in data[0]) {
+      productInfo[key] = data[0][key];
+    }
+  });
+
+  let features = [];
+  db.query(`SELECT * FROM features WHERE product_id = ${req.params.product_id}`, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    // Populate features array
+    data.forEach((obj) => {
+      const featuresObj = {};
+      featuresObj.feature = obj.feature;
+      featuresObj.value = obj.value;
+      features.push(featuresObj);
+    });
+    // Build response obj and send to client
+    productInfo.features = features;
+    res.send(productInfo);
   });
 });
 
-// Return array of product_id's related to current product
-app.get('/products/:product_id/related', (req, res) => {
-  db.query(`SELECT current_product_id, related_product_id FROM relatedProducts WHERE current_product_id = ${req.params.product_id}`, (err, data) => {
-    if(err) {
+// Return obj containing all available styles for current product
+app.get('/products/:product_id/styles', (req, res) => {
+  let styles = {};
+  styles.product_id = req.params.product_id;
+  let results = [];
+  db.query(`SELECT * FROM styles WHERE productId = ${req.params.product_id}`, (err, data) => {
+    if (err) {
       console.log(err);
     }
-    res.send(data);
+    console.log(data);
+    // For each obj in data, create a corresponding styleObj and push to results
+    data.forEach((obj) => {
+      let styleObj = {
+        style_id: obj.id,
+        name: obj.name,
+        original_price: obj.original_price,
+        sale_price: obj.sale_price ? obj.sale_price : '0',
+        'default?': obj.default_style === 0,
+        // photos: [],
+        // skus: {}
+      };
+      // Populate photos array with obj{thumbnail_url, url} from photos table
+      let photos = [];
+      db.query(`SELECT * FROM photos WHERE styleId = ${obj.id}`, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('PHOTOS FOR THIS STYLE ID:', data);
+        data.forEach((obj) => {
+          let photoObj = {
+            thumbnail_url: '',
+            url: ''
+          };
+        });
+      });
+      // Populate skus obj
+    });
+  });
+  res.send(styles);
+});
+
+// Return array of product_ids related to current product
+app.get('/products/:product_id/related', (req, res) => {
+  db.query(`SELECT CAST(CONCAT('[', GROUP_CONCAT(related_product_id SEPARATOR ','), ']') AS JSON ) FROM relatedProducts WHERE current_product_id = ${req.params.product_id}`, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    const key = Object.keys(data[0])[0];
+    res.send(data[0][key]);
   });
 });
 
@@ -50,6 +113,9 @@ app.listen(app.get('port'), () => {
 });
 
 // TODO: Look into Supertest for testing. Call supertest functions from within Jest
+
+// Example query to return array of related_product_ids
+// `SELECT array_agg(related.related_product_id) AS result FROM product LEFT JOIN (SELECT current_product_id, related_product_id FROM related) AS related ON product.id=related.current_product_id WHERE id=${id} GROUP BY product.id`
 
 // Example query to return data as an array:
 // SELECT
@@ -71,4 +137,3 @@ app.listen(app.get('port'), () => {
 //       GROUP BY t1.tag_id, t2.tag_name
 //   ) AS TAGS)
 // FROM dev_timelogs;
-
